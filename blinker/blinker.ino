db57a9bcb21400c49afaaa9a68b62a60b179bccf
johannesno99 <lidvor99@hotmail.com>
2023-09-14 15:35:32 +0200

    adds button

--- blinker/blinker.ino
+++ blinker/blinker.ino
@@ -1,8 +1,27 @@
+const int LED_PIN = 13;
+const int BUTTON_PIN = 2;
+
+bool buttonState = false;
+
+void updateButton(){
+    buttonState = digitalRead(BUTTON_PIN);
+}
+
+void blinkLed(){
+    digitalWrite(LED_PIN, HIGH);
+    delay(200);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+}
+
 void setup(){
     Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT);
     }
 
 void loop(){
-    Serial.println("a");
-    delay(1000);
-    }
+     blinkLed();
+     updateButton();
+     Serial.println(buttonState);
+}
